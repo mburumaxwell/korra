2eefd985e4a4ef89e81c21f420aa802a2b3b80a0
Maxwell Weru <1645026+mburumaxwell@users.noreply.github.com>
2025-06-18 15:40:53 +0100

    Group logs for build

--- firmware/scripts/firmware.mjs
+++ firmware/scripts/firmware.mjs
@@ -3,13 +3,14 @@
 import { Argument, Command } from 'commander';
 import { existsSync } from 'node:fs';
 import { mkdir, copyFile, rm, writeFile } from 'node:fs/promises';
-import { execSync } from 'node:child_process';
+import { execSync, spawn, spawnSync } from 'node:child_process';
 import { join } from 'node:path';
 import process from 'node:process';
 import * as semver from 'semver';
 
 import packageJson from '../../package.json' with { type: "json" };
 
+const isCI = Boolean(process.env['CI'] || process.env['GITHUB_ACTIONS']);
 const KNOWN_APPS = ['keeper', 'pot'];
 const KNOWN_BOARDS = [
   'esp32s3_devkitc/esp32s3/procpu',
@@ -32,7 +33,7 @@ const version = new Command('version')
       throw new Error('Invalid version in package.json');
     }
 
-    const validateByte = (num, label) =>  (num >= 0 && num <= 255) || (() => { throw new Error(`${label} must be in range 0-255`); })();
+    const validateByte = (num, label) => (num >= 0 && num <= 255) || (() => { throw new Error(`${label} must be in range 0-255`); })();
 
     let tweak = version.build.length > 0 ? parseInt(version.build[0], 10) : 0;
     let extraversion =
@@ -79,11 +80,12 @@ const build = new Command('build')
   .action(async (...args) => {
     const [app, board, options/*, command*/] = args;
     const buildDir = makeBuildDir({ app, board });
+    if (isCI) console.log(`::group::Building app: ${app} for: ${board}`);
     console.log(`🚀 Building app: ${app}, targeting: ${board}`);
     const { pristine } = options;
 
     // if we are not in CI and the local file exists, mark it for inclusion
-    const local = !Boolean(process.env['CI'] || process.env['GITHUB_ACTIONS']) && existsSync('local.conf');
+    const local = !isCI && existsSync('local.conf');
 
     const cmd = [
       'west build',
@@ -96,12 +98,13 @@ const build = new Command('build')
     ].filter(Boolean).join(' ');
     console.log(`🏗️ Running: ${cmd}`);
 
-    try {
-      execSync(cmd, { stdio: 'inherit' });
-      console.log(`✅ Build completed for app: ${app}, targeting: ${board}`);
-    } catch (error) {
-      console.error(`❌ Build failed:`, error.message);
-      process.exit(1);
+    const result = spawnSync(cmd, { shell: true, stdio: 'inherit' });
+    console.log(`✅ Build completed for app: ${app}, targeting: ${board}`);
+
+    if (isCI) console.log(`::endgroup::`);
+
+    if (result.status !== 0) {
+      process.exit(result.status);
     }
   });
 
