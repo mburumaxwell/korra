d8536d7fad2b1d7ce230a181aacbf06847772912
Maxwell Weru <1645026+mburumaxwell@users.noreply.github.com>
2025-06-19 20:22:13 +0100

    Set hostname which would be used for mDNS

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -2,6 +2,7 @@
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 #include <zephyr/kernel.h>
+#include <zephyr/net/hostname.h>
 #include <app_version.h>
 
 #include "korra_internet.h"
@@ -11,6 +12,7 @@ int main(void)
 	// Log application version matching Kernel: *** Booting Zephyr OS build v4.1.0 ***
 	printk("*** Booting Korra %s build v%s (%s) ***\n", CONFIG_APP_NAME, APP_VERSION_STRING, STRINGIFY(APP_BUILD_VERSION));
 	printk("*** Running Board %s at %d MHz ***\n", CONFIG_BOARD, SystemCoreClock / MHZ(1));
+	printk("*** Hostname: %s ***\n", net_hostname_get());
 
 	korra_internet_init();
 	korra_internet_connect();
