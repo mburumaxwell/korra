090bb0f6df55130992f8ded5e2d042a1b90dcaf6
Maxwell Weru <1645026+mburumaxwell@users.noreply.github.com>
2025-06-19 19:59:58 +0100

    Log board name and speed

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -10,6 +10,7 @@ int main(void)
 {
 	// Log application version matching Kernel: *** Booting Zephyr OS build v4.1.0 ***
 	printk("*** Booting Korra %s build v%s (%s) ***\n", CONFIG_APP_NAME, APP_VERSION_STRING, STRINGIFY(APP_BUILD_VERSION));
+	printk("*** Running Board %s at %d MHz ***\n", CONFIG_BOARD, SystemCoreClock / MHZ(1));
 
 	korra_internet_init();
 	korra_internet_connect();
