e2aeb630d33606677cab7ba5338aa27d9ed7b066
Maxwell Weru <1645026+mburumaxwell@users.noreply.github.com>
2025-06-23 09:00:35 +0100

    Change sensors module to use a timer and delayable work queue just like time sync

--- firmware/src/sensors/korra_sensors.c
+++ firmware/src/sensors/korra_sensors.c
@@ -6,11 +6,12 @@ LOG_MODULE_REGISTER(korra_sensors, LOG_LEVEL_INF);
 
 #include "korra_sensors.h"
 
-#define DATA_COLLECTION_INTERVAL K_SECONDS(CONFIG_DATA_COLLECTION_INTERVAL_SECONDS)
+#define READ_INTERVAL K_SECONDS(CONFIG_SENSORS_READ_INTERVAL_SECONDS)
 
+static struct k_timer collect_data_timer;
 static struct k_work_delayable collect_data_work;
+static void collect_data_timer_handler(struct k_timer *timer);
 static void collect_data_work_handler(struct k_work *work);
-static void collect_data();
 
 void korra_sensors_init()
 {
@@ -18,20 +19,22 @@ void korra_sensors_init()
 
     // TODO: setup sensors here (checking if the devices are ready and any specific setup)
 
-    // Initialize work item for collecting data and schedule first read
+    // Initialize timer and work item for data collection
+    k_timer_init(&collect_data_timer, collect_data_timer_handler, NULL);
     k_work_init_delayable(&collect_data_work, collect_data_work_handler);
-    k_work_schedule(&collect_data_work, DATA_COLLECTION_INTERVAL); // should this be K_NO_WAIT?
+
+    // Start the timer
+    k_timer_start(&collect_data_timer, READ_INTERVAL, READ_INTERVAL); // should we start with K_NO_WAIT instead?
 }
 
-static void collect_data()
+static void collect_data_work_handler(struct k_work *work)
 {
     LOG_INF("Collecting data");
 
     // TODO: collect actual sensors data
 }
 
-static void collect_data_work_handler(struct k_work *work)
+static void collect_data_timer_handler(struct k_timer *timer)
 {
-    collect_data();
-    k_work_schedule(&collect_data_work, DATA_COLLECTION_INTERVAL); // reschedule
+    k_work_schedule(&collect_data_work, K_NO_WAIT);
 }
