054bb9928179a3a0a3f844ba7385b8707d539cb6
Maxwell Weru <mburumaxwell@gmail.com>
2025-07-09 09:29:20 +0100

    Formatting fixes

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,7 +1,7 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
 
 import { Provider } from '@/components';
 import './globals.css';
