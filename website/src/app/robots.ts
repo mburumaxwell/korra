db50d102c60bb25a5fe4c587e220318178738d42
Maxwell Weru <mburumaxwell@gmail.com>
2025-07-10 00:05:07 +0100

    Merge robots.ts as only one should exist

--- website/src/app/robots.ts
+++ website/src/app/robots.ts
@@ -4,7 +4,11 @@ import { config } from '@/site-config';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: '*' }],
+    rules: [
+      { userAgent: '*', allow: '/' },
+      // dashboard should not be crawled
+      { userAgent: '*', disallow: '/dashboard' },
+    ],
     sitemap: [`${config.siteUrl}/sitemap.xml`],
     host: config.siteUrl,
   };
