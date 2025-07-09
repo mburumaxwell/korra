2bddb422fff40fa68667e94c81c51d8c13f41759
Maxwell Weru <mburumaxwell@gmail.com>
2025-07-09 08:59:22 +0100

    Setup vercel speed-insights and analytics

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import { Provider } from '@/components';
 import './globals.css';
@@ -28,6 +30,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Provider>{children}</Provider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
