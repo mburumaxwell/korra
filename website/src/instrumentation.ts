41eaba02deb8225a538593193ae952d2f973c25b
Maxwell Weru <mburumaxwell@gmail.com>
2025-07-09 08:56:40 +0100

    Skip adding azure monitor to Otel when deployed to vercel

--- website/src/instrumentation.ts
+++ website/src/instrumentation.ts
@@ -4,7 +4,8 @@ import { registerOTel } from '@vercel/otel';
 export async function register() {
   let traceExporter: SpanExporter | undefined;
 
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
+  const isVercelDeployment = Boolean(process.env.VERCEL_DEPLOYMENT_ID);
+  if (!isVercelDeployment && process.env.NEXT_RUNTIME === 'nodejs') {
     const { AzureMonitorTraceExporter } = await import('@azure/monitor-opentelemetry-exporter');
     traceExporter = new AzureMonitorTraceExporter({
       connectionString: process.env.NEXT_PUBLIC_APP_INSIGHTS_CONNECTION_STRING,
